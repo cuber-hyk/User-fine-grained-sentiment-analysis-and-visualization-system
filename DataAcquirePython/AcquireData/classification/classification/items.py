8824ff6255dda238d65de459564ec9a295a6544c
73bnUHT <2369365233@qq.com>
2025-02-22 11:27:09 +0800

    scrapy

---
+++ DataAcquirePython/AcquireData/classification/classification/items.py
@@ -0,0 +1,53 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+# best seller下各个类别的href和类名title
+class HrefItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    classification = scrapy.Field()
+    href = scrapy.Field()
+    pass
+
+
+# 各个类别下的二级href和类名title
+class SecondHrefItem(scrapy.Item):
+    classification = scrapy.Field()
+    href = scrapy.Field()
+    first_classification = scrapy.Field()
+    pass
+
+
+class PidItem(scrapy.Item):
+    pid = scrapy.Field()
+    classification = scrapy.Field()
+    pass
+
+
+class Product(scrapy.Item):
+    pid = scrapy.Field()
+    name = scrapy.Field()
+    stars = scrapy.Field()
+    discount_price = scrapy.Field()
+    normal_price = scrapy.Field()
+    ratings = scrapy.Field()
+    about_this_item = scrapy.Field()
+    classification = scrapy.Field()
+    channel = scrapy.Field()
+    image_url = scrapy.Field()
+
+
+class Review(scrapy.Item):
+    user = scrapy.Field()
+    pid = scrapy.Field()
+    title = scrapy.Field()
+    rating = scrapy.Field()
+    post_time = scrapy.Field()
+    content = scrapy.Field()
+    style = scrapy.Field()
+
---
+++ DataAcquirePython/AcquireData/classification/classification/settings.py
@@ -0,0 +1,124 @@
+# Scrapy settings for classification project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import scrapy.pipelines.images
+
+BOT_NAME = "classification"
+
+SPIDER_MODULES = ["classification.spiders"]
+NEWSPIDER_MODULE = "classification.spiders"
+
+
+DOWNLOAD_DELAY = 2
+RANDOMIZE_DOWNLOAD_DELAY = True
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = {
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0"
+}
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#    "Accept-Language": "en",
+#}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "Pid.middlewares.PidSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    "Pid.middlewares.PidDownloaderMiddleware": 543,
+#}
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   "classification.pipelines.ClassificationPipeline": 300,
+}
+
+IMAGES_STORE = 'E:/analysis-reviews-data-picture'
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
+
+# Database - Mysql
+MYSQL_HOST = 'localhost'
+MYSQL_DATABASE = 'analysis_reviews'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'gdlsd030312'
+MYSQL_PORT = 3306
+
+# 启用自定义的随机延迟中间件
+DOWNLOADER_MIDDLEWARES = {
+    'classification.middlewares.RandomDelayMiddleware': 543,
+    'classification.middlewares.NoRedirectMiddleware': 600,
+}
+
+# 设置随机延迟范围（单位：秒）
+RANDOM_DELAY_RANGE = (0, 0.5)  # 在1到5秒之间随机选择延迟
+
+# 处理 302 try
+
+HTTPERROR_ALLOWED_CODES = [302]  # 允许处理302
+DUPEFILTER_DEBUG = True          # 调试重复请求
+FOLLOW_REDIRECTS = False
+COOKIES_ENABLED = True
+COOKIES_DEBUG = True  # 调试Cookies传递
+
